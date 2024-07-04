03dcea715cd284f58f81e395b1a3833fe7154ebf
Dimitra Perissiou <dimitraper85@gmail.com>
2024-07-04 17:06:46 +0200

    Basic functions added - destroy function is still not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'lists/index'
+  get 'lists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +10,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
+
+# A user can add a new bookmark (movie/list pair) to an existing list
+# Checkout simple_form docs about f.association to easily create a select dropdown for our list of movies.
+# GET "lists/42/bookmarks/new"
+# POST "lists/42/bookmarks"
+# A user can delete a bookmark from a list. How can we make a delete link again?
+# DELETE "bookmarks/25"
+
+# As a user, I can see all my movie lists - index ✅
+# As a user, I can create a movie list - show ✅
+# As a user, I can see the details of a movie list -new and create ✅
+# As a user, I can bookmark a movie inside a movie list -new and create (nested in lists)
+# As a user, I can destroy a bookmark - destroy (not nested)
